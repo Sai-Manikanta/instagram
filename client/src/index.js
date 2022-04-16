b4c0d56b5b0dc48da7898feea127889a802ac31a
mani333007 <mani333007@gmail.com>
2022-04-16 18:00:32 +0530

    Pagination, added Home and Login basic pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App';
 import UserContextProvider from './contexts/UserContextProvider'
@@ -11,14 +12,16 @@ const domainID = process.env.REACT_APP_AUTH0_DOMAIN_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Auth0Provider
-          domain={domainID}
-          clientId={clientID}
-          redirectUri={window.location.origin}
-        >
-          <UserContextProvider>
-            <App />
-          </UserContextProvider>
-        </Auth0Provider>
+        <BrowserRouter>
+          <Auth0Provider
+            domain={domainID}
+            clientId={clientID}
+            redirectUri={window.location.origin}
+          >
+            <UserContextProvider>
+              <App />
+            </UserContextProvider>
+          </Auth0Provider>
+        </BrowserRouter>
     </React.StrictMode>
 )
