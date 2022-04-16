d7a002a7e8e62acb5c7c2f80b4bdcac0673fd817
mani333007 <mani333007@gmail.com>
2022-04-16 18:25:37 +0530

    favicon

--- client/src/pages/LoadingScreen.js
+++ client/src/pages/LoadingScreen.js
@@ -2,7 +2,7 @@ const LoadingScreen = () => {
     return(
         <div className="h-screen bg-gray-100 flex justify-center items-center border">
             <img 
-                src="https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png" 
+                src="icons/android-icon-72x72.png" 
                 alt="logo" 
                 className="w-14 h-14"
             />
