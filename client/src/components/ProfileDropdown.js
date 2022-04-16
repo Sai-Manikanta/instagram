75046a901454df7b5ef9b3897bac54af8ab2084e
mani333007 <mani333007@gmail.com>
2022-04-16 19:58:06 +0530

    profile dropdown

--- client/src/components/ProfileDropdown.js
+++ client/src/components/ProfileDropdown.js
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { CgProfile } from 'react-icons/cg'
+import { MdOutlineLabel } from 'react-icons/md'
+import { IoSettingsOutline } from 'react-icons/io5'
+import { useAuth0 } from '@auth0/auth0-react'
 import { useUserContext } from './../contexts/UserContextProvider'
 
 const ProfileDropdown = () => {
     const [showDropdown, setShowDropdown] = useState(false);
     const { user } = useUserContext();
+
+    const { logout } = useAuth0();
 
     return(
         <div className="relative">
@@ -15,10 +22,22 @@ const ProfileDropdown = () => {
             />
             {showDropdown && (
                 <div 
-                    className="p-4 w-64 shadow-md rounded absolute bg-white right-0 top-12"
+                    className="w-64 shadow-md rounded absolute bg-white right-0 top-12 text-sm"
                     data-testid="profile-dropdown"
                 >
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti ratione modi, aspernatur minima esse optio cum consectetur praesentium rem corporis eius placeat aut culpa ea hic autem facilis dolorem?</p>
+                    <Link to="/profile" className="hover:bg-gray-100 w-full py-2 px-3 flex items-center">
+                        <CgProfile className="mr-2" /> Profile
+                    </Link>
+                    <Link to="/saved" className="hover:bg-gray-100 w-full py-2 px-3 flex items-center">
+                        <MdOutlineLabel className="mr-2" /> Saved
+                    </Link>
+                    <Link to="/settings" className="hover:bg-gray-100 w-full py-2 px-3 flex items-center">
+                        <IoSettingsOutline className="mr-2" /> Settings
+                    </Link>
+                    <hr />
+                    <button onClick={() => logout()} className="hover:bg-gray-100 w-full py-2 px-3 text-left flex items-center">
+                        Log Out
+                    </button>
                 </div>
             )}
         </div>
